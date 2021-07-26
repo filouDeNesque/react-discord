aab8225ce53b93fa267653949a5bed3d8c0788bf
filouDeNesque <p.cuquemy@gmail.com>
2021-07-26 17:15:53 +0200

    app config import header & navbarleft

--- react-discord/src/App.js
+++ react-discord/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import navBarLeft from "./NavBarLeft";
+import header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <navBarLeft></navBarLeft>
+      </div>
+      <div>
+        <header></header>
+      </div>
     </div>
   );
 }
