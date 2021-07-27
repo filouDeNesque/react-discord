1362ff68d264284409940e56053a9a56de17ffaa
FilouDeNesque <johndoe@example.com>
2021-07-27 14:40:27 +0200

    css

--- react-discord/src/Main.js
+++ react-discord/src/Main.js
@@ -1,28 +1,35 @@
 import messages from "./Message.json";
 export default function Main() {
   return (
-    <><div className="message">
-      <div class="message-padding">
-        {messages.map((user) => (
-          <div class="body-message">
-            <img src={user.image} alt=""></img>
-            <div class="main-message">
-              <div class="header-message">
-                <h4>{user.utilisateur}</h4>
-                <p>{user.Date}</p>
-              </div>
-              <div class="bottom-message">
-                <p>{user.Message}</p>
+    <>
+      <div className="message">
+        <div class="message-padding">
+          {messages.map((user) => (
+            <div class="body-message">
+              <img src={user.image} alt=""></img>
+              <div class="main-message">
+                <div class="header-message">
+                  <h4>{user.utilisateur}</h4>
+                  <p>{user.Date}</p>
+                </div>
+                <div class="bottom-message">
+                  <p>{user.Message}</p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <TextField />
       </div>
-      <TextField />
-    </div></>
+    </>
   );
 }
 
-function TextField(){
-  return <textarea class="text-area" />;
+function TextField() {
+  return (
+    <>
+   
+      <textarea class="text-area" />
+    </>
+  );
 }
