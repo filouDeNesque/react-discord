633fc0aa813b61258164a5214cece5eeb7894513
FilouDeNesque <p.cuquemy@yahoo.com>
2021-08-26 19:37:15 +0200

    mise en place du projet

---
+++ portfolio/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className="App">
+    <Header />
+
+    {/* 
+    TODO:section 1 presentation
+    TODO: section 2 more about me
+    TODO; section 3 techno utilisé
+    TODO: section 4 projets
+    TODO: contact */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+function App() {
+  return (
+    <div className="Header">
+    <h2>Paul</h2>
+    {/* TODO:Menu flottant
+      TODO: contact bar (github, linkedin, mail)*/}
+    </div>
+  );
+}
+
+export default Header;
